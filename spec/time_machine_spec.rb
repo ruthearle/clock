7ca4133ec4c3ddd9076c3843a235f0a8d51d9648
mariogintili <mariojgintili@gmail.com>
2015-03-16 15:36:22 +0000

    failing integration test for PUT /api/time written.

--- spec/time_machine_spec.rb
+++ spec/time_machine_spec.rb
@@ -9,22 +9,48 @@ describe TimeMachine::API do
     TimeMachine::API
   end
 
-  before do
-    @the_time = Time.now
-    Timecop.freeze(@the_time)
-  end
-
-  after do
-    Timecop.return
-  end
-
   describe TimeMachine::API do
+
     describe "GET /api/time" do
+
+      before do
+        @current_time = Time.now
+        Timecop.freeze(@current_time)
+      end
+
+      after do
+        Timecop.return
+      end
 
       it 'returns the time in JSON format' do
         get "api/time"
         expect(last_response.status).to eq 200
-        expect(JSON.parse(last_response.body)).to eq ({ "time" => "#{@the_time}" })
+        expect(JSON.parse(last_response.body)).to eq ({ "time" => "#{@current_time}" })
+      end
+    end
+
+    describe "PUT /api/time" do
+
+      before do
+        @new_time = Time.now + 3600
+        Timecop.freeze(@new_time)
+      end
+
+      after do
+        Timecop.return
+      end
+
+      new_time = { "time" => @new_time }
+
+      request_headers = {
+       "Accept" => "application/json",
+       "Content-Type" => "application/json"
+      }
+
+      it "allows a user to alter the time" do
+        put "api/time", new_time, request_headers
+        expect(last_response.status).to eq 200
+        expect(ApiTime.get).to match new_time
       end
     end
   end
