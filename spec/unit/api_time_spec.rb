33c264d27dd5535ab93dae09e4ad0c0be0c6f5d2
mariogintili <mariojgintili@gmail.com>
2015-03-17 14:44:50 +0000

    tested and passed ApiTime#get to return current time or updated time

--- spec/unit/api_time_spec.rb
+++ spec/unit/api_time_spec.rb
@@ -5,19 +5,35 @@ describe ApiTime do
 
   describe "#get" do
     it "returns the current time" do
-      @current_time = Time.now
-      allow(Time).to receive(:now).and_return(@current_time)
+      current_time = Time.now
+      allow(Time).to receive(:now).and_return(current_time)
       expect(ApiTime.get).to match Time.now
+    end
+
+    it "returns the current time or the adjusted time" do
+      current_time = Time.now
+      allow(Time).to receive(:now).and_return(current_time)
+      expect(ApiTime.get).to match Time.now
+
+      time = (Time.now + 3600)
+      ApiTime.update(time)
+      expect(ApiTime.get).to match time
     end
   end
 
   describe "#update" do
-    it "allows the current time to be changed" do
+    before do
       @updated_time = Time.now + 3600
-      Timecop.freeze(@updated_time) do
-        ApiTime.update(@updated_time)
-        expect(ApiTime.get).to match @updated_time
-      end
+      Timecop.freeze(@updated_time)
+    end
+
+    after do
+      Timecop.return
+    end
+
+    it "allows the current time to be changed" do
+      ApiTime.update(@updated_time)
+      expect(ApiTime.get).to match @updated_time
     end
   end
 end
